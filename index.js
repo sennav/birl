595f7da0b13055649ecd39335ee7a60891e8acf1
Vinicius <senna.vmd@gmail.com>
2019-09-08 20:10:47 +0200

    É CLI ESSA PORRA

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 #! /usr/bin/env node
 const fs = require('fs');
 const { birlToJs } = require('./birl.js');
+const commander = require('commander');
 
 function main() {
-  const birlFilepath = process.argv[2];
+  let birlFilepath;
+  const program = new commander.Command();
+
+  program
+    .version('BIRL 1.0.0')
+    .option('-j, --javascript', 'MOSTRA ESSE JAVASCRIPT TREZE!')
+    .arguments('<SEU PROGRAMA FRANGO>')
+    .action((filename) => {
+      birlFilepath = filename;
+    });
+  program.parse(process.argv);
+
+  if (!birlFilepath) {
+    program.outputHelp();
+    process.exit(1);
+  }
+
   const birlCode = fs.readFileSync(birlFilepath, 'utf8');
   const jsCode = birlToJs(birlCode);
+
+  if (program.javascript) {
+    console.log(jsCode);
+    return;
+  }
   eval(jsCode);
 }
 
