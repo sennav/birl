8ce9427183b479780ae58e0f12363d51496befd3
Vinicius Delgado <vinicius@stravito.com>
2019-12-01 19:19:23 +0100

    UAAAAAAHHH SAIU DA JAULA A VERSÃO 1.0.1, BIRL!!!

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function main() {
   const program = new commander.Command();
 
   program
-    .version('BIRL 1.0.0')
+    .version('BIRL 1.0.1')
     .option('-j, --javascript', 'MOSTRA ESSE JAVASCRIPT TREZE!')
     .arguments('<SEU_PROGRAMA_FRANGO>')
     .action((filename) => {
